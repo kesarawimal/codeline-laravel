3d6a74c6380e9c7886d3d12c16442b811e749a5d
Kesara Wimal <kesarawimal9@gmail.com>
2018-10-27 16:20:42 +0530

    1.1 & 1.2 backend

---
+++ database/migrations/2018_10_27_102354_create_genres_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGenresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('genres', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on(User::USER)->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('genres');
+    }
+}
